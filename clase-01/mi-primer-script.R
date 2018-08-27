aa87a963802d0b7426b184ee62173ab6e713cf35
jbkunst <jbkunst@gmail.com>
2018-08-27 19:13:30 -0300

    manual riva

--- clase-01/mi-primer-script.R
+++ clase-01/mi-primer-script.R
@@ -1,2 +1,25 @@
 modlin <- lm(dist ~ speed, data = cars)
 plot(modlin)
+
+summary(modlin)
+
+x <- rnorm(n = 1000, mean = 0, sd = 1)
+
+hist(x)
+
+plot(x)
+
+plot(cumsum(x), type = "l")
+
+mean(x)
+
+# install.packages("highcharter")
+
+highcharter::hchart(hist(x))
+
+highcharter::hchart(AirPassengers)
+
+highcharter::hchart(forecast::forecast(AirPassengers))
+
+highcharter::hchart(forecast::forecast(log(AirPassengers)))
+
--- clase-01/readme.md
+++ clase-01/readme.md
@@ -1,6 +1,6 @@
 # Clase 01
 
-- Instalación de R+RStudio, paquetes
+- Instalación de R+RStudio, paquetes: https://github.com/rivaquiroga/RLadies-Santiago/blob/master/2018-04_taller_primeros_pasos_en_R.Rmd
 - Lectura de datos
 - Manejo de tablas dplyr
 - Visualización
