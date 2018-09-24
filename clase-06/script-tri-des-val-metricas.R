e22577e3189b9842dc0acb9f74bf7f7ecf9f45a0
jbkunst <jbkunst@gmail.com>
2018-09-24 19:44:24 -0300

    asd

--- clase-06/script-tri-des-val-metricas.R
+++ clase-06/script-tri-des-val-metricas.R
@@ -1,5 +1,51 @@
 # paquetes ----------------------------------------------------------------
 library(tidyverse)    # manipular y graficar
-library(ModelMetrics) # 
-library(partykit)
-library(smbinning)
+library(partykit)     # Arboles
+library(ModelMetrics) # metricias de performance
+library(smbinning)    # Datos
+
+# install.packages("smbinning") # chileno
+
+# chileandcredit<- readRDS("https://github.com/cran/smbinning/blob/master/data/chileancredit.RData?raw=true")
+
+# datos -------------------------------------------------------------------
+data("chileancredit")
+
+chileancredit
+
+# Primero deberemos separar la """tabla"""
+# P: Por que?
+# Para genera un arbol(modelo), saber como _generaliza_
+
+# P: Como separo la tabla?
+# R: Al azar, para que cada una de las muestras
+# sea parecida, en terminos de las caracteristicas
+# de las variables.
+# No producir zezgoz
+
+# P: Como hago algo al azar.
+# comando sample
+
+sample(1:10, 3)
+
+set.seed(123)
+sample(1:10, 3)
+
+set.seed(123)
+para_donde_se_va <- sample(
+ c("para aka", "pallá"),
+ size = 10000,
+ prob = c(0.5, 0.5),
+ replace = TRUE
+)
+
+table(para_donde_se_va)
+
+chileancredit <- chileancredit %>% 
+  mutate(para_donde_se_va = para_donde_se_va)
+
+ddes <- chileancredit %>% 
+  filter(para_donde_se_va == "para aka")
+# 4943
+
+
