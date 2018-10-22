f3e8e8c012ccd44b26bdacf5ed128e3ae5b92e1f
jbkunst <jbkunst@gmail.com>
2018-10-22 19:09:00 -0300

    Subiendo datos a pedido de Pablo

---
+++ clase-12/script.R
@@ -0,0 +1,9 @@
+# packages ----------------------------------------------------------------
+library(tidyverse)
+library(partykit)
+library(randomForest)
+
+# data --------------------------------------------------------------------
+data <- read_csv("clase-12/data/hour.csv")
+
+data
