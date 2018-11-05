5f9c7d59febb21dc1f3407021adf5a1d917f6970
jbkunst <jbkunst@gmail.com>
2018-11-05 19:45:19 -0300

    para el que estaba poniendo atencion :)

--- clase-14/script.R
+++ clase-14/script.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+library(lubridate)
 
 empresa <- "CHILE"
 # Para que hacemos lo anterior? 
@@ -12,3 +12,22 @@ url <- paste(
   "-days.txt",
   sep = ""
 )
+
+data <- read_csv(url)
+
+glimpse(data)
+
+# Tenemos los datos, grafiquemos el volumen tranzado
+ggplot(data) +
+  geom_line(aes(`<DTYYYYMMDD>`, `<VOL>`))
+
+# :)
+data <- data %>% 
+  mutate(fecha = ymd(`<DTYYYYMMDD>`))
+
+glimpse(data)
+
+ggplot(data) +
+  geom_line(aes(fecha, `<OPEN>`))
+
+
